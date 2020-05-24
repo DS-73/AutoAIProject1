ad0f5a9cf1a72ea88efa353bb47bff7b47ee897f
DS-73 <dhruvsaini73@gmail.com>
2020-05-24 15:02:03 +0530

    changed

---
+++ DockerL.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import os
+def check_if_string_in_file(file_name, string_to_search):
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            if string_to_search in line:
+                return True
+    return False
+
+# Searching For CNN 
+if check_if_string_in_file('Text.txt', 'Conv2D'):
+    os.system('echo "Yes, string found in file" ')
+    os.system('sudo docker -divt /root/AutoAIProject1:/root/ --name Python_OS CNN_Python')
+
+# Searching for Linear Regression
+if check_if_string_in_file('sample.txt', 'LinearRegression'):
+    os.system('echo "Yes, string found in file" ')
+    os.system('sudo docker -divt /root/AutoAIProject1:/root/ --name Python_OS Linear_Python')
+
+else:
+    os.system('echo "Yes, string found in file" ')
+    os.system('sudo docker -divt /root/AutoAIProject1:/root/ --name Python_OS Basic_Python')
+
+
+# In[ ]:
+
+
+
+
